c64bc82c1389a1081d4b711da78d83a9b33cc9a1
shunkat <shunkat@icloud.com>
2022-03-02 11:14:49 +0900

    フロントをカオスに

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,10 @@
 <template>
   <div>
-    <div class="mainVisual">
-      <picture>
-        <source
-          srcset="https://placehold.jp/375x530.png"
-          media="(max-width: 767px)"
-        />
-        <img src="https://placehold.jp/1440x436.png" alt="" />
-      </picture>
-    </div>
     <!-- ここから自分の情報に編集してください -->
     <section id="introduction" class="primary">
       <div class="container">
-        <h2 class="middleTitle">自己紹介</h2>
         <div class="profile">
+          <h2 class="middleTitle">自己紹介</h2>
           <h1 class="name">＜名前をここに＞</h1>
           <div class="tech">
             <h3>技術領域</h3>
@@ -23,15 +14,16 @@
             <h3>趣味</h3>
             <p>＜趣味をここに＞</p>
           </div>
-          <figure class="selfee">
-            <img src="https://placehold.jp/260x260.png" alt="your name" />
-          </figure>
+          <h3>どんな人</h3>
           <p class="detailintroduction">
             ＜こまかい自己紹介はここに入れてみましょう＞<br />
             例えば、どうしてエンジニアになったのかorなりたいのかなど<br />
             もし思いつかなければ好きなご飯のおかずを書いてください。
           </p>
         </div>
+        <figure class="selfee">
+          <img src="/yourIcon.jpg" width=260 height=260 alt="your name" />
+        </figure>
       </div>
     </section>
     <!-- ここまで編集 -->
@@ -42,36 +34,66 @@
         <!-- ここからコメントアウト外す -->
         <div class="products">
           <div v-for="content in contents.contents" :key="content.id" class="eachProduct">
-            <nuxt-link :to="`/works/${content.id}/`" class="productWrapper">
-              <div class="title">
+            <nuxt-link :to="`/works/${content.id}/`">
+              <div class="productWrapper">
+                <div class="title">
                 <p class="works__name">{{ content.title }}</p>
+                </div>
+                <figure class="productImage">
+                  <img
+                    :width="260"
+                    :height="260"
+                    :src="content.capture.url"
+                    :alt="content.title"
+                  />
+                </figure>
               </div>
-              <figure class="productImage">
-                <img
-                  :width="260"
-                  :height="260"
-                  :src="content.capture.url"
-                  :alt="content.title"
-                />
-              </figure>
+              
             </nuxt-link>
           </div>
         </div>
         <!-- ここまでコメントアウト外す -->
-        <p class="button-area">
-          <nuxt-link to="/contact/" class="button">お問い合わせ</nuxt-link>
-        </p>
       </div>
+      
     </section>
+    <div class="button-area">
+          <nuxt-link to="/contact/" class="button">お問い合わせ</nuxt-link>
+      </div>
   </div>
 </template>
 
 <style lang="css" scoped>
+body {
+  width: 1250px;
+}
 .mainVisual img {
   width: 100%;
 }
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+#works {
+  background-color: orange;
+}
 .products{
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+}
+.eachProduct{
+  width: 50%;
+}
+.productWrapper{
+  display: flex;
+  flex-wrap: wrap;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto 
 }
 </style>
 
@@ -79,8 +101,7 @@
 export default {
   async asyncData({ $microcms }) {
     const contents = await $microcms.get({
-      endpoint: 'products',
-      queries: { limit: 2 },
+      endpoint: 'products'
     })
     return {
       contents,
