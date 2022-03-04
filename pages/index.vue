e1275eb6d205cbfd059681504af810c51fc13e6c
shunkat <shunkat@icloud.com>
2022-03-04 18:20:20 +0900

    画像の更新

--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,24 @@
       <div class="container">
         <div class="profile">
           <h2 class="middleTitle">自己紹介</h2>
-          <h1 class="name">＜名前をここに＞</h1>
+          <h1 class="name">「名前をここに」</h1>
           <div class="tech">
             <h3>技術領域</h3>
-            <p>＜技術スタックをここに＞</p>
+            <p>「技術スタックをここに」</p>
           </div>
           <div class="hobby">
             <h3>趣味</h3>
-            <p>＜趣味をここに＞</p>
+            <p>「趣味をここに」</p>
           </div>
           <h3>どんな人</h3>
           <p class="detailintroduction">
-            ＜こまかい自己紹介はここに入れてみましょう＞<br />
+            「こまかい自己紹介はここに」<br />
             例えば、どうしてエンジニアになったのかorなりたいのかなど<br />
             もし思いつかなければ好きなご飯のおかずを書いてください。
           </p>
         </div>
         <figure class="selfee">
-          <img src="/yourIcon.jpg" width=260 height=260 alt="your name" />
+          <img src="/yourIcon.jpg" width=390 height=390 alt="your name" />
         </figure>
       </div>
     </section>
@@ -31,6 +31,22 @@
     <section id="works" class="primary">
       <div class="container">
         <h2 class="middleTitle">制作物</h2>
+
+        <!-- ここからコメントアウト -->
+        <div class="products">
+          <div v-for="i of 3" :key="i" class="eachProduct">
+            <nuxt-link :to="`/works/`">
+              <div class="productWrapper">
+                <p class="productTitle">サンプルプロダクト{{ i }}</p>
+                <figure class="productImage">
+                  <img :src="i+'.png'" :width="260" :height="260" :alt="sampleImageOfProduct">
+                </figure>
+              </div>
+            </nuxt-link>
+          </div>
+        </div>
+        <!-- ここまでコメントアウト -->
+        
         <!-- ここからコメントアウト外す -->
         <!-- <div class="products">
           <div v-for="content in contents.contents" :key="content.id" class="eachProduct">
@@ -52,46 +68,7 @@
           </div>
         </div> -->
         <!-- ここまでコメントアウト外す -->
-        <!-- ここからコメントアウト -->
-        <div class="products">
-          <div class="eachProduct">
-            <nuxt-link :to="`/works/`">
-              <div class="productWrapper">
-                <div class="title">
-                <p class="productTitle">サンプルプロダクト</p>
-                </div>
-                <figure class="productImage">
-                  <img width="260" height="260" src="/markingCloud.png" alt="sampleImageOfProduct" />
-                </figure>
-              </div>
-            </nuxt-link>
-          </div>
-          <div class="eachProduct">
-            <nuxt-link :to="`/works/`">
-              <div class="productWrapper">
-                <div class="title">
-                <p class="productTitle">サンプルプロダクト2</p>
-                </div>
-                <figure class="productImage">
-                  <img width="260" height="260" src="/isoroot_logo.png" alt="sampleImageOfProduct" />
-                </figure>
-              </div>
-            </nuxt-link>
-          </div>
-          <div class="eachProduct">
-            <nuxt-link :to="`/works/`">
-              <div class="productWrapper">
-                <div class="title">
-                <p class="productTitle">サンプルプロダクト3</p>
-                </div>
-                <figure class="productImage">
-                  <img width="260" height="260" src="/google.png" alt="sampleImageOfProduct" />
-                </figure>
-              </div>
-            </nuxt-link>
-          </div>
-        </div>
-        <!-- ここまでコメントアウト -->
+        
       </div>
       
     </section>
