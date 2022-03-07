74db4d933737ce1abfa3297261a85a2c319e5383
kato.shun <kato.shun@isoroot.jp>
2022-03-08 02:27:58 +0900

    ハンズオン用のブランチ作成

--- pages/index.vue
+++ pages/index.vue
@@ -13,36 +13,23 @@
         class="ma-12 rounded-circle"
       />
       <!-- 自己紹介 -->
-      <p class="text-h3">牧 クラウド</p>
+      <p class="text-h3">「1. 名前」</p>
       <p class="text-h6 text--secondary mb-12">
-        「こまかい自己紹介はここに」<br />
+        「2. 細かい自己紹介」<br />
         例えば、どうしてエンジニアになったのか or なりたいのかなど。<br />
         もし思いつかなければ好きなご飯のおかずを書いてください。
       </p>
 
       <!-- スキル -->
       <p class="text-h4">スキル</p>
-      <p class="text-h6 text--secondary mb-12">技術スタックをここに</p>
+      <p class="text-h6 text--secondary mb-12">「3. 技術スタック」</p>
 
       <!-- 趣味 -->
       <p class="text-h4">趣味</p>
-      <p class="text-h6 text--secondary mb-12">趣味をここに</p>
+      <p class="text-h6 text--secondary mb-12">「4. 趣味」</p>
 
       <!-- 制作物 -->
       <p class="text-h4">制作物</p>
-
-      <!-- ここからコメントアウト -->
-      <v-row class="ma-12">
-        <v-col cols="4" class="px-12" v-for="i of 3" :key="i">
-          <nuxt-link :to="`/works/`">
-            <v-card elevation="2" outlined>
-              <v-card-title>サンプルプロダクト{{ i }}</v-card-title>
-              <v-img :src="i + '.png'" height="240" />
-            </v-card>
-          </nuxt-link>
-        </v-col>
-      </v-row>
-      <!-- ここまでコメントアウト -->
 
       <!-- ここからコメントアウト外す -->
       <!-- <v-row class="ma-12">
@@ -53,7 +40,7 @@
           :key="content.id"
         >
           <nuxt-link :to="`/works/${content.id}/`">
-            <v-card elevation="2" outlined>
+            <v-card elevation="2" height="200">
               <v-card-title>{{ content.title }}</v-card-title>
               <v-img :src="content.capture.url" height="240" />
             </v-card>
